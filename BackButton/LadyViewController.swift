5fa9d85fd7111507667c177295b3ead71d0ed366
Steve Lipton <stevelipton@brk-16892.local>
2018-04-26 06:15:33 -0700

    initial commit final state

---
+++ BackButton/LadyViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LadyViewController.swift
+//  BackButton
+//
+//  Created by Steve Lipton on 4/26/18.
+//  Copyright © 2018 Steve Lipton. All rights reserved.
+//
+
+import UIKit
+
+class LadyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BackButton/ViewController.swift
+++ BackButton/ViewController.swift
@@ -2,24 +2,38 @@
 //  ViewController.swift
 //  BackButton
 //
-//  Created by Steve Lipton on 4/26/18.
-//  Copyright © 2018 Steve Lipton. All rights reserved.
+//  Created by Steven Lipton on 4/22/18.
+//  Copyright © 2018 Steven Lipton. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        title = "Gallery"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        title = "Gallery"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //title = "Return to Gallery"
+        title = ""
+        if segue.identifier == "Lady"{
+            let vc = segue.destination as! LadyViewController
+            vc.title = "Lady Watercolor"
+        }
     }
-
-
+    
+    
 }
+
+
+
+
+
+
 
